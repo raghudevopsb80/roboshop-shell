ff365fa3b7fb4ba51a4d7ae958da903ef08625b9
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-03 06:49:03 +0530

    RoboShop redis script

---
+++ user.sh
@@ -0,0 +1,13 @@
+cp user.service /etc/systemd/system/user.service
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
