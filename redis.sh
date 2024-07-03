17e74a642411949bb622f66fcc98c9952a662a1e
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-03 07:16:21 +0530

    Redis update

--- redis.sh
+++ redis.sh
@@ -4,4 +4,4 @@ dnf install redis -y
 sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
