aa3a9d960a2631648df6e8b71d10934fc66c5ac8
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-03 06:24:17 +0530

    RoboShop frontend script

---
+++ frontend.sh
@@ -0,0 +1,11 @@
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+# Update config file
+systemctl restart nginx
