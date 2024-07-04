0489b7a5f36dcd85877c8058320abb50daa9a35e
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-04 06:40:53 +0530

    Redis update

--- catalogue.sh
+++ catalogue.sh
@@ -17,4 +17,4 @@ systemctl enable catalogue
 systemctl start catalogue
 
 dnf install mongodb-mongosh -y
-mongosh --host localhost </app/db/master-data.js
+mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js
