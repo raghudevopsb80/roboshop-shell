3cadbf9ecfbc1b34479e18adc63dd78b690ae144
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-04 06:58:03 +0530

    Cart service

--- cart.sh
+++ cart.sh
@@ -2,6 +2,7 @@ cp cart.service /etc/systemd/system/cart.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+useradd roboshop
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
--- user.sh
+++ user.sh
@@ -2,6 +2,7 @@ cp user.service /etc/systemd/system/user.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+useradd roboshop
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 cd /app
