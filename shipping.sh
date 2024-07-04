3e2811ff322c74087eddaa85a582bb32e10770ab
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-04 07:10:16 +0530

    Shipping Setup

--- shipping.sh
+++ shipping.sh
@@ -11,9 +11,9 @@ mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
 dnf install mysql -y
-mysql -h mysql.dev.rdevopb80.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql.dev.rdevopb80.online -uroot -pRoboShop@1 < /app/db/master-data.sql
-mysql -h mysql.dev.rdevopb80.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h mysql.dev.rdevopsb80.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql.dev.rdevopsb80.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql.dev.rdevopsb80.online -uroot -pRoboShop@1 < /app/db/app-user.sql
 
 systemctl daemon-reload
 systemctl enable shipping
