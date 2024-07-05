100c004f9f7a7be2e3ead002aa8f228400918245
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-05 07:29:05 +0530

    Shell script

--- common.sh
+++ common.sh
@@ -1,41 +1,49 @@
-LOG_FILE=/tmp/roboshop.log 
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+
+PRINT() {
+  echo &>>$LOG_FILE
+  echo &>>$LOG_FILE
+  echo " ####################################### $* ########################################" &>>$LOG_FILE
+  echo $*
+}
 
 NODEJS() {
-  echo Disable NodeJS Default Version
+  PRINT Disable NodeJS Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
 
-  echo Enable NodejS 20 Module
+  PRINT Enable NodejS 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
 
-  echo Install Nodejs
+  PRINT Install Nodejs
   dnf install nodejs -y &>>$LOG_FILE
 
-  echo Copy Service file
+  PRINT Copy Service file
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
 
-  echo COpy MongoDB repo file
+  PRINT COpy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 
-  echo Adding Application User
+  PRINT Adding Application User
   useradd roboshop &>>$LOG_FILE
 
-  echo Cleaning Old Content
+  PRINT Cleaning Old Content
   rm -rf /app &>>$LOG_FILE
 
-  echo Create App directory
+  PRINT Create App directory
   mkdir /app &>>$LOG_FILE
 
-  echo Download App Content
+  PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
 
   cd /app
-  echo Extract App Content
+  PRINT Extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
 
-  echo Download NodeJS Dependencies
+  PRINT Download NodeJS Dependencies
   npm install &>>$LOG_FILE
 
-  echo Start Service
+  PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl restart ${component} &>>$LOG_FILE
