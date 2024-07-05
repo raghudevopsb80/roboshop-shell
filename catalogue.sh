d89aa1776da13876745aac7d510b64b1bb6d64fc
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-05 07:24:59 +0530

    Shell script

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ component=catalogue
 NODEJS
 
 echo Install MongoDB Client
-dnf install mongodb-mongosh -y &>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 
 echo Load Master Data
-mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js &>$LOG_FILE
+mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js &>>$LOG_FILE
--- common.sh
+++ common.sh
@@ -2,43 +2,43 @@ LOG_FILE=/tmp/roboshop.log
 
 NODEJS() {
   echo Disable NodeJS Default Version
-  dnf module disable nodejs -y &>$LOG_FILE
+  dnf module disable nodejs -y &>>$LOG_FILE
 
   echo Enable NodejS 20 Module
-  dnf module enable nodejs:20 -y &>$LOG_FILE
+  dnf module enable nodejs:20 -y &>>$LOG_FILE
 
   echo Install Nodejs
-  dnf install nodejs -y &>$LOG_FILE
+  dnf install nodejs -y &>>$LOG_FILE
 
   echo Copy Service file
-  cp ${component}.service /etc/systemd/system/${component}.service &>$LOG_FILE
+  cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
 
   echo COpy MongoDB repo file
-  cp mongo.repo /etc/yum.repos.d/mongo.repo &>$LOG_FILE
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 
   echo Adding Application User
-  useradd roboshop &>$LOG_FILE
+  useradd roboshop &>>$LOG_FILE
 
   echo Cleaning Old Content
-  rm -rf /app &>$LOG_FILE
+  rm -rf /app &>>$LOG_FILE
 
   echo Create App directory
-  mkdir /app &>$LOG_FILE
+  mkdir /app &>>$LOG_FILE
 
   echo Download App Content
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>$LOG_FILE
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
 
   cd /app
   echo Extract App Content
-  unzip /tmp/${component}.zip &>$LOG_FILE
+  unzip /tmp/${component}.zip &>>$LOG_FILE
 
   echo Download NodeJS Dependencies
-  npm install &>$LOG_FILE
+  npm install &>>$LOG_FILE
 
   echo Start Service
-  systemctl daemon-reload &>$LOG_FILE
-  systemctl enable ${component} &>$LOG_FILE
-  systemctl restart ${component} &>$LOG_FILE
+  systemctl daemon-reload &>>$LOG_FILE
+  systemctl enable ${component} &>>$LOG_FILE
+  systemctl restart ${component} &>>$LOG_FILE
 
 }
 
