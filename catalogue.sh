417a8fb22167fbc0a2a893caa49e08fff6de33b0
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-05 07:21:11 +0530

    Shell script

--- catalogue.sh
+++ catalogue.sh
@@ -2,5 +2,8 @@ source common.sh
 component=catalogue
 NODEJS
 
-dnf install mongodb-mongosh -y
-mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js
+echo Install MongoDB Client
+dnf install mongodb-mongosh -y &>$LOG_FILE
+
+echo Load Master Data
+mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js &>$LOG_FILE
