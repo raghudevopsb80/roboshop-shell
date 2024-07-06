7cfdd6940dadc3343eaf4dc9b31c1e138fcc5874
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-06 06:55:52 +0530

    Shell script

--- redis.sh
+++ redis.sh
@@ -1,7 +1,21 @@
+
+PRINT Disbale redis default
 dnf module disable redis -y
+STAT $?
+
+PRINT Enable redis 7
 dnf module enable redis:7 -y
+STAT $?
+
+PRINT Install Redis 7
 dnf install redis -y
-sed -i '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+STAT $?
+
+PRINT Update Redis Config
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+STAT $?
+
+PRINT Start Redis Service
 systemctl enable redis
 systemctl restart redis
+STAT $?
