cfe77b8856276f8d2080d2301941b4b1b225c493
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-06 07:01:19 +0530

    Shell script

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,17 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl restart mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+source common.sh
+component=mongo
+
+PRINT Install MySQL Server
+dnf install mysql-server -y &>>$LOG_FILE
+STAT $?
+
+PRINT Start MySQL Service
+systemctl enable mysqld &>>$LOG_FILE
+systemctl restart mysqld &>>$LOG_FILE
+STAT $?
+
+PRINT Setup MySQL Root Password
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+STAT $?
+
+
