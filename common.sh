2e58c15d31f9e34f557b150a3a9de36e88390599
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-06 06:37:45 +0530

    Shell script

--- common.sh
+++ common.sh
@@ -13,6 +13,8 @@ STAT() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
+    echo
+    echo "Refer the log file for more information : File Path : ${LOG_FILE}"
     exit $1
   fi
 }
