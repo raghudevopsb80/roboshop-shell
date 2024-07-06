07196c41ca812ce0e36db1534dc8ba98313e24b2
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-06 06:15:50 +0530

    Shell script

--- catalogue.sh
+++ catalogue.sh
@@ -4,6 +4,8 @@ NODEJS
 
 echo Install MongoDB Client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
+echo $?
 
 echo Load Master Data
 mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js &>>$LOG_FILE
+echo $?
--- common.sh
+++ common.sh
@@ -11,42 +11,54 @@ PRINT() {
 NODEJS() {
   PRINT Disable NodeJS Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
+  echo $?
 
   PRINT Enable NodejS 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
+  echo $?
 
   PRINT Install Nodejs
   dnf install nodejs -y &>>$LOG_FILE
+  echo $?
 
   PRINT Copy Service file
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
+  echo $?
 
   PRINT COpy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+  echo $?
 
   PRINT Adding Application User
   useradd roboshop &>>$LOG_FILE
+  echo $?
 
   PRINT Cleaning Old Content
   rm -rf /app &>>$LOG_FILE
+  echo $?
 
   PRINT Create App directory
   mkdir /app &>>$LOG_FILE
+  echo $?
 
   PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
+  echo $?
 
   cd /app
   PRINT Extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
+  echo $?
 
   PRINT Download NodeJS Dependencies
   npm install &>>$LOG_FILE
+  echo $?
 
   PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl restart ${component} &>>$LOG_FILE
+  echo $?
 
 }
 
