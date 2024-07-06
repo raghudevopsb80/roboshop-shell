434f5ba60983155e0d4f03206e41a47e3c590d0f
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-06 06:52:28 +0530

    Shell script

--- mongo.sh
+++ mongo.sh
@@ -1,5 +1,19 @@
+source common.sh
+component=mongo
+
+PRINT Copy MongoDB repo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+STAT $?
+
+PRINT Install MongoDB
 dnf install mongodb-org -y
+STAT $?
+
+PRINT Update MongoDB config file
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+PRINT Start MongoDB Service
 systemctl enable mongod
 systemctl restart mongod
+STAT $?
