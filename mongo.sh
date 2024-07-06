e7260b0e1fad1310ead0cc03cdfdde370c94513a
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-06 06:57:27 +0530

    Shell script

--- mongo.sh
+++ mongo.sh
@@ -2,18 +2,18 @@ source common.sh
 component=mongo
 
 PRINT Copy MongoDB repo file
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 STAT $?
 
 PRINT Install MongoDB
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOG_FILE
 STAT $?
 
 PRINT Update MongoDB config file
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
 STAT $?
 
 PRINT Start MongoDB Service
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable mongod &>>$LOG_FILE
+systemctl restart mongod &>>$LOG_FILE
 STAT $?
--- redis.sh
+++ redis.sh
@@ -2,22 +2,22 @@ source common.sh
 component=redis
 
 PRINT Disbale redis default
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 STAT $?
 
 PRINT Enable redis 7
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 STAT $?
 
 PRINT Install Redis 7
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 STAT $?
 
 PRINT Update Redis Config
-sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 STAT $?
 
 PRINT Start Redis Service
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis &>>$LOG_FILE
+systemctl restart redis &>>$LOG_FILE
 STAT $?
