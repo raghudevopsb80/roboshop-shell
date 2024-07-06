f0ac83ae643470522aa01d2638703dbea5e2226c
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-06 06:27:00 +0530

    Shell script

--- catalogue.sh
+++ catalogue.sh
@@ -4,16 +4,8 @@ NODEJS
 
 echo Install MongoDB Client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
 
 echo Load Master Data
 mongosh --host mongo.dev.rdevopsb80.online </app/db/master-data.js &>>$LOG_FILE
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-fi
+STAT $?
--- common.sh
+++ common.sh
@@ -8,105 +8,65 @@ PRINT() {
   echo $*
 }
 
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
+}
+
 NODEJS() {
   PRINT Disable NodeJS Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Enable NodejS 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Install Nodejs
   dnf install nodejs -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Copy Service file
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT COpy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Adding Application User
   useradd roboshop &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Cleaning Old Content
   rm -rf /app &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Create App directory
   mkdir /app &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   cd /app
   PRINT Extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Download NodeJS Dependencies
   npm install &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl restart ${component} &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
 }
 
